16c8aec0e35c6b4b6a305a99fd9917d345349b3c
emilbratt <emilbratt@gmail.com>
2020-05-10 12:02:02 +0200

    Lagt til totalsum + sum eks moms på begge valgene

--- Momskalkulator.py
+++ Momskalkulator.py
@@ -7,8 +7,8 @@ def momsTotalSum(momsProsent, sumVerdi):
     return momsVerdi
 
 brukerValg = 0
-print('Tast 1 for å regne ut moms fra totalsum,\
- tast 2 for å regne ut moms fra sum eks moms.')
+print('Tast 1 for å regne ut moms fra en sum eks. moms. \n\
+Tast 2 for å regne ut moms fra en totalsum.')
 brukerValg = int(input())
 
 
@@ -16,10 +16,13 @@ if brukerValg == 1:
     print('Skriv inn momsen du ønsker\
  å regne med (for eskempel 25 for 25%)')
     brukerProsent = int(input())
-    print('Skriv inn summen du skal regne momsen fra.')
+    print('Skriv inn summen du skal regne ut momsen for.')
     brukerSum = int(input())
     svarMoms = momsUtenTotal(brukerProsent, brukerSum)
-    print(float("{:.2f}".format(svarMoms)))
+    print('Av ' + str(brukerSum) + 'kr blir momsen ' + str(float("{:.2f}".format(svarMoms)))\
+ + 'kr.')
+    totalSum = brukerSum + svarMoms
+    print('Totalsummen blir ' + str(totalSum) + 'kr.')
 elif brukerValg == 2:
     print('Skriv inn momsen du ønsker\
  å regne med (for eskempel 25 for 25%)')
@@ -27,6 +30,9 @@ elif brukerValg == 2:
     print('Skriv inn summen du skal regne momsen fra.')
     brukerSum = int(input())
     svarMoms = momsTotalSum(brukerProsent, brukerSum)
-    print(float("{:.2f}".format(svarMoms)))
+    print('Av totalsum på ' + str(brukerSum) +  'kr, utgjør momsen '\
+ + str(float("{:.2f}".format(svarMoms))) + 'kr.')
+    sumUtenMoms = brukerSum - svarMoms
+    print('Sum eks. moms blir da ' + str(sumUtenMoms) + 'kr.')
 else:
     print('Du har ikke valgt 1 eller 2.')
